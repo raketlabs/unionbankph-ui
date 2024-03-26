eb88186ce08ef2f139192008b81f528a50a03ba0
ral <raketlabs.com@gmail.com>
2024-03-26 16:52:32 +0800

    show logo and fade to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '@app/login/login.component';
+import { SplashComponent } from '@app/splash/splash.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: ``, component: SplashComponent },
+    { path: `login`, component: LoginComponent },
+];
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './splash.component.html',
   styleUrl: './splash.component.scss'
 })
-export class SplashComponent {
+export class SplashComponent implements OnInit {
+
+  isLoaded = false
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // do work
+    setTimeout(() => this.done(), 5000)
+  }
+
+  done() {
+    this.isLoaded = true
+    setTimeout(() => this.router.navigate(['/login']), 200)
+  }
 
 }
